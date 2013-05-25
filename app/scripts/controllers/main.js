03298a2f49523787dc3e954642ad8b0cc41b7b5a
Nicola Peduzzi <thenikso@gmail.com>
2013-05-25 15:56:25 +0200

    Initialized Yeoman Angular app with ZURB Foundation

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('UnionCarWebsiteApp')
+  .controller('MainCtrl', ['$scope', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  }]);
