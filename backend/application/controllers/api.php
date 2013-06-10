45105c3efa9ab6e93ce5a6393271cc1acedbac31
Nicola Peduzzi <thenikso@gmail.com>
2013-06-10 10:26:18 +0200

    Added Codeigniter to backend

---
+++ backend/application/controllers/api.php
@@ -0,0 +1,11 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Api extends CI_Controller {
+
+	public function car($id = null) {
+		$this->output
+			->set_content_type('application/json')
+			->set_output(json_encode(array('foo' => 'bar', 'id' => $id)));
+	}
+
+}
