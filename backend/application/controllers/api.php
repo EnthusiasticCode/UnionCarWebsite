fe3f95e96047e1628cc8ec86984e1b27f5ec4f4a
Nicola Peduzzi <thenikso@gmail.com>
2013-06-10 11:13:33 +0200

    Added CSVReader php library

--- backend/application/controllers/api.php
+++ backend/application/controllers/api.php
@@ -3,6 +3,9 @@
 class Api extends CI_Controller {
 
 	public function car($id = null) {
+		// $this->load->library('unzip');
+		// $this->load->library('csvreader');
+
 		$this->output
 			->set_content_type('application/json')
 			->set_output(json_encode(array('foo' => 'bar', 'id' => $id)));
